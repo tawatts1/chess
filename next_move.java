a1802db535cd0bdc2aa689336e3255abd99466fc
Ted Watts <tedawatts@gmail.com>
2020-11-02 20:35:13 -0700

    The moves appear correct without checking for if they can jump over other pieces. I will implement that for queen, pawn, rook etc now.

--- next_move.java
+++ next_move.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-import java.util.List;
+
 
 /** 
  * "br_00_bb_bq_bk_bb_bn_br=bp_bp_bp_bp_bp_bp_bp_bp=00_00_bn_00_00_00_00_00=00_00_00_00_00_00_00_00=00_00_00_wp_00_00_00_00=00_00_00_00_wp_00_00_00=wp_wp_wp_00_00_wp_wp_wp=wr_wn_wb_wq_wk_wb_wn_wr"
@@ -14,11 +14,11 @@ public class next_move
   public static void main (String[] args)
   {
     //int M = args.size();
-    //ArrayList<ArrayList<String>> board0 = construct_board(args[0]);
-    char[][][] board = construct_board(args[0]);
-    byte[] mv0 = {0,2};
     
-    ArrayList<byte[]> mvs = moves_pre_check(board, mv0, 'b');
+    char[][][] board = construct_board(args[0]);
+    byte[] mv0 = {6,0};
+    
+    ArrayList<byte[]> mvs = moves_pre_check(board, mv0, 'w');
     
     print_board(board);
     for (byte[] mv : mvs)
@@ -32,7 +32,6 @@ public class next_move
   public static ArrayList<byte[]> moves_pre_check(char[][][] board, byte[] coords, char color)
   {
     ArrayList<byte[]> out = new ArrayList<byte[]>();
-    //byte[][] out = {{0,0},{0,0}};
     char piece = board[coords[0]][coords[1]][1];
     char piece_color = board[coords[0]][coords[1]][0];
     if (piece_color==color)
@@ -54,6 +53,7 @@ public class next_move
             }
           }
           break;
+
         case 'b':
           byte[][] basis = {{1,-1},{1,1}};
           byte[] sign = {-1,1};
@@ -66,9 +66,7 @@ public class next_move
               {
                 byte alpha = (byte) (multiplier[i]*sgn);
                 byte[] move = multiply_list(base, alpha);
-                System.out.print(move[0] + ", "+ move[1]);
                 byte[] out0 = add_lists(coords, move);
-                System.out.println(" ... " + out0[0] + ", "+ out0[1]);
                 if (in_board_space(out0))
                 {
                   //byte[][] mv = {coords, out0};
@@ -90,9 +88,7 @@ public class next_move
               {
                 byte alpha = (byte) (multiplier1[i]*sgn);
                 byte[] move = multiply_list(base, alpha);
-                System.out.print(move[0] + ", "+ move[1]);
                 byte[] out0 = add_lists(coords, move);
-                System.out.println(" ... " + out0[0] + ", "+ out0[1]);
                 if (in_board_space(out0))
                 {
                   //byte[][] mv = {coords, out0};
@@ -102,11 +98,75 @@ public class next_move
             }
           }
           break;
+        case 'q':
+          byte[][] basis2 = {{1,0},{0,1}, {-1,1},{1,1}};
+            byte[] sign2 = {-1,1};
+            byte[] multiplier2 = {1,2,3,4,5,6,7,8};
+            for (byte sgn : sign2)
+            {
+              for (byte[] base : basis2)
+              {
+                for (byte i=0; i<8; i++)
+                {
+                  byte alpha = (byte) (multiplier2[i]*sgn);
+                  byte[] move = multiply_list(base, alpha);
+                  byte[] out0 = add_lists(coords, move);
+                  if (in_board_space(out0))
+                  {
+                    //byte[][] mv = {coords, out0};
+                    out.add(out0);
+                  }
+                }
+              }
+            }
+            break;
+
+        case 'k':
+          byte[][] basis3 = {{1,0},{0,1}, {-1,1},{1,1}};
+          byte[] sign3 = {-1,1};
+          //byte[] multiplier3 = {1};
+          for (byte sgn : sign3)
+          {
+            for (byte[] base : basis3)
+            {
+            
+              //byte alpha = (byte) (multiplier3[i]*sgn);
+              byte[] move = multiply_list(base, sgn);
+              byte[] out0 = add_lists(coords, move);
+              if (in_board_space(out0))
+              {
+                //byte[][] mv = {coords, out0};
+                out.add(out0);
+              }
+            
+            }
+          }
+          break;
+        case 'p':
+          byte sgn = get_pawn_sign(piece_color);
+          byte[] mv0 = {sgn, 0};
+          byte[] out0 = add_lists(coords, mv0);
+          out.add(out0);
+          break;
+        
       }// end switch statement
     }
 
     return out;
   }
+
+
+
+
+
+
+
+
+
+
+
+
+
   private static boolean in_board_space(byte[] out0)
   {
     if (out0[0]<8 && out0[0]>-1 && out0[1] < 8 && out0[1] >-1){return true;}
@@ -166,6 +226,22 @@ public class next_move
       }
       System.out.println(row_str);
     }
+  }
+  private static byte get_pawn_sign(char color)
+  {
+    byte out = 0;
+    if (color=='b'){out = 1;}
+    else if (color=='w'){out = -1;}
+    return out;
+  }
+  private static byte friendly_fire(char[][][] board, byte[] target_coord, char color)
+  {
+    byte out;
+    char target_color = board[target_coord[0]][target_coord[1]][0];
+    if (target_color == '0'){out = 0;}
+    else if (target_color == color){out = 1;}
+    else {out = 1;}
+    return out;
   }
 
 }
