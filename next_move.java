13caaa640b23bb6cd5b7a50d5e7747e8b0424ed4
Ted Watts <tedawatts@gmail.com>
2020-11-06 21:11:23 -0700

    Added the empty piece as a private static final shared variable. I will try to do this for all variables later if I need more speed.

--- next_move.java
+++ next_move.java
@@ -11,6 +11,7 @@ import java.util.Random;
 
 public class next_move
 {
+  private static final char[] EMPTY = {'0','0'};
   public static void main(String[] args)
   {
     
@@ -91,14 +92,14 @@ public class next_move
       }
       else
       {
-        board1[i] = board[i];//.clone();//.clone(); // no clone
+        board1[i] = board[i]; // no clone
       }
       
     }
-    char[] empty = {'0','0'};
+    //char[] empty = {'0','0'};
     char[] piece = board1[c1[0]][c1[1]];
     board1[c2[0]][c2[1]] = piece;
-    board1[c1[0]][c1[1]] = empty;
+    board1[c1[0]][c1[1]] = EMPTY;
     return board1;
   }
   private static ArrayList<byte[][]> get_moves(char[][][] board, char color)
