128aafc3c3210d2ca2b2ad348a757bb79f03c60c
Ted Watts <tedawatts@gmail.com>
2020-12-09 08:49:16 -0700

    Cut down Square.py to only what is necessary. I removed the initial game setup from game_setup.py

--- Square.py
+++ Square.py
@@ -11,32 +11,21 @@ def none_callback():
     print('None callback')
 
 class Square(tk.Button):
-    def __init__(self, parent, img = None, color = 'grey', 
-                 index = None, occupant = None):
+    def __init__(self, parent, fname = None, color = 'yellow'):
         super().__init__(parent)
-        
+
         self.parent = parent
-        self.index = tuple(index)
-        self.occupant = occupant
+        self.fname = fname
         
-        if img is None:
+        if fname is None:
             photo = tk.PhotoImage()
-        else:    
-            photo = tk.PhotoImage(file = img)
-        
-        if occupant == None:
-            self.config(image = photo, bg = color, 
-                                height = 60, width = 60)#, command = none_callback)
         else:
-            self.config(image = photo, bg = color, 
-                                height = 60, width = 60)#, command = self.get_moves)
-            
+            photo = tk.PhotoImage(file = fname)
+        
+        self.config(image = photo, bg = color, 
+                            height = 60, width = 60)
         self.image = photo
-        self.grid(row=0,column=0)
-        self.command = None #self.command to be used later
-    
-    def change_occupant(self, new_occupant):
-        self.occupant = new_occupant
+        self.grid(row=0, column=0)
     def change_photo(self, fname):
         if fname is not None:
             photo = tk.PhotoImage(file = fname)
@@ -51,5 +40,5 @@ class Square(tk.Button):
 if __name__ == '__main__':
     
     root = tk.Tk()
-    s1 = Square(root, img = 'images/bq.png')
+    s1 = Square(root)
     root.mainloop()
--- game_setup.py
+++ game_setup.py
@@ -25,50 +25,13 @@ def index_to_piece(index):
 
 
 def piece_to_fname(piece):
-    if piece is None:
+    if piece is ['0','0']:
         return None
-    elif len(piece) == 2:
-        if piece[0] in 'bw':
+    elif piece[0] in 'bw':
             if piece[1] in 'kqbnrp':
-                return 'images/' + piece + '.png'
-    return None
+                return 'images/' + piece[0] + piece[1] + '.png'
 def index_to_fname(index, piece_f = piece_to_fname):
     return piece_f(  index_to_piece(index)  )
-
-def standard_game(root):
-        board0 = []
-        boardstr = []
-        sq_dict = {}
-        blist = []
-        wlist = []
-        for i in range(8):
-            row = []
-            rowstr = []
-            for j in range(8):
-                full_name = index_to_piece([i,j])
-                fname = piece_to_fname(full_name)
-                coords0 = np.array([i,j])
-                if isinstance(full_name, str):
-                    occupant0 = full_name
-                    # add to white or black list:
-                    {'b':blist,
-                        'w':wlist}[full_name[0]].append(coords0)
-                else:
-                    occupant0 = None
-                sq = Square(root,
-                            img = fname,
-                            index = coords0,
-                            occupant = occupant0)
-                #sq.config(command = sq.get_moves)
-                sq_dict[tuple(coords0)] = sq
-                #squares.append(sq)
-                sq.grid(row=i, column=j)
-                row.append(sq)
-                rowstr.append(occupant0)
-            board0.append(row)
-            boardstr.append(rowstr)
-        
-        return board0, boardstr, sq_dict
 
 
 
